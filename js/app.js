a7f7597d3a610fc52c061e15193556f59cb40250
mithon42 <mithonphilip@gmail.com>
2023-10-03 19:04:03 +0600

    professional skills circle added

--- js/app.js
+++ js/app.js
@@ -41,3 +41,27 @@ let changeText = () => {
 changeText();
 setInterval(changeText, 3000);
 // Changes Text Ends #####################
+
+
+
+//  Circle Skills Start #####################
+const circles = document.querySelectorAll('.circle');
+circles.forEach(elem => {
+   let dots = elem.getAttribute('data-dots');
+   let marked = elem.getAttribute('data-percent');
+   let percent = Math.floor(dots*marked/100);
+   let points = "";
+   let rotate = 360 / dots;
+
+   for(let i = 0; i < dots; i++){
+      points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`
+   }
+   elem.innerHTML = points;
+
+   const pointsMarked = elem.querySelectorAll('.points');
+   for(let i = 0; i < percent; i++){
+      pointsMarked[i].classList.add('marked')
+   };
+});
+
+//  Circle Skills Ends #####################
